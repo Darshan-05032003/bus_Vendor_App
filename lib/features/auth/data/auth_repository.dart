48ada01ad630e3630d2346368b3d2564563c0c40
Darshan Sonawane <sonawanedarshan35@gmail.com>
2026-04-16 15:21:03 +0530

    feat: Integrate Firebase authentication and storage support in vendor_app

--- lib/features/auth/data/auth_repository.dart
+++ lib/features/auth/data/auth_repository.dart
@@ -1,4 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:vendor_app/features/auth/domain/vendor_user.dart';
+import 'package:vendor_app/core/services/local_storage.dart';
+
 class AuthRepository {
-  Future<void> signInWithGoogle() async {}
-  Future<void> signInWithPhone(String phone) async {}
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final LocalStorageService _localStorage;
+
+  AuthRepository(this._localStorage);
+
+  //--Helper to convert Fireabase User to our Domain User--
+  VendorUser? _mapFirebaseUser(User? user) {
+    if (user == null) return null;
+    return VendorUser(
+      uid: user.uid,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      displayName: user.displayName,
+    );
+  }
+
+  //--listen to auth state changes--
+  Stream<VendorUser?> get authStateChanges =>
+      _auth.authStateChanges().map(_mapFirebaseUser);
+
+  //--1.Google Sign-In-
+  Future<VendorUser?> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _goggleSignIn.signIn();
+      if (googleUser == null) return null; //user canceled the sign-in
+
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+      final OAuthCredential credential = GoogleAuthProvider.credentail(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final 
+    } catch (e) {}
+  }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_analytics
+import firebase_auth
 import firebase_core
+import firebase_messaging
+import firebase_storage
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
